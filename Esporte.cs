6a1111525e1b79e8499bf4d525fb4ffbb559d944
Luiza <102320472+luvarella@users.noreply.github.com>
2022-12-15 13:19:14 -0300

    Add files via upload

---
+++ Esporte.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetopeootema14
+{
+    class Esporte
+    {
+        private string nome, horarios;
+        private double mensalidade;
+    }
+}
