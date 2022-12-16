87224a00e929d103889c16eb11b25ee2245f3312
luvarella <luvarellagois@gmail.com>
2022-12-16 08:36:49 -0300

    atualização

---
+++ Projeto PEOO 3bi/Projeto PEOO 3bi/Esporte.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_PEOO_3bi
+{
+    internal class Esporte
+    {
+        private string nome, horarios;
+        private double mensalidade;
+        public void SetNome(string n)
+        {
+            nome = n;
+        }
+        public void SetHorarios(string h)
+        {
+            horarios = h;
+        }
+        public void SetMensalidade(double m)
+        {
+            if (m >= 0) mensalidade = m;
+        }
+        public string GetNome()
+        {
+            return nome;
+        }
+        public string GetHorarios()
+        {
+            return horarios; 
+        }
+        public double GetMensalidade()
+        {
+            return mensalidade;
+        }
+    }
+}
